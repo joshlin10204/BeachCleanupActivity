24455c01b2d820119c74976fcc4e422d6efc440d
JoshLin <>
2018-07-22 17:39:28 +0800

    上傳App

---
+++ BeachCleanupActivity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeachCleanupActivity
+//
+//  Created by JoshLin on 2018/7/22.
+//  Copyright © 2018年 JoshLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeachCleanupActivity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeachCleanupActivity
+//
+//  Created by JoshLin on 2018/7/22.
+//  Copyright © 2018年 JoshLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
